aecb6db342e617637aa46651c22e0281fc0375d3
Valeria Fomina <v.fomina01@mail.ru>
2023-02-19 17:21:34 +0300

    Fixed image loading

--- app/src/main/java/com/example/filmcatalog/data/Film.kt
+++ app/src/main/java/com/example/filmcatalog/data/Film.kt
@@ -1,12 +1,14 @@
 package com.example.filmcatalog.data
 
+import com.example.filmcatalog.R
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 data class Film(
     val id: Int,
     val name: String,
-    val photo_id: Int,
+    @Transient var photo_id: Int = R.drawable.ic_launcher_foreground,
     val date_publication: String,
     val rating: Double,
     val description: String,
--- app/src/main/java/com/example/filmcatalog/viewmodel/FilmsViewModel.kt
+++ app/src/main/java/com/example/filmcatalog/viewmodel/FilmsViewModel.kt
@@ -3,15 +3,29 @@ package com.example.filmcatalog.viewmodel
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.example.MyApplication
+import com.example.filmcatalog.R
 import com.example.filmcatalog.data.Film
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 class FilmsViewModel : ViewModel() {
     fun getFilms() =
-        Json.decodeFromString<List<Film>>(loadJsonFromAssets(MyApplication.getInstance()))
+        Json
+            .decodeFromString<List<Film>>(loadJsonFromAssets(MyApplication.getInstance()))
+            .mapIdToImageId()
 
     fun getFilmById(filmId: Int): Film? = getFilms().find { it.id == filmId }
+
+    private fun List<Film>.mapIdToImageId() = map { film ->
+        when (film.id) {
+            0 -> film.photo_id = R.drawable.film_img_0
+            1 -> film.photo_id = R.drawable.film_img_1
+            2 -> film.photo_id = R.drawable.film_img_2
+            3 -> film.photo_id = R.drawable.film_img_3
+            4 -> film.photo_id = R.drawable.film_img_4
+            5 -> film.photo_id = R.drawable.film_img_5
+        }; film
+    }
 
     private fun loadJsonFromAssets(context: Context): String {
         var result = ""
